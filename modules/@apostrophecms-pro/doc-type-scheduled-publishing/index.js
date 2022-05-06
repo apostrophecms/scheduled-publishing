211d3b20942e01fb025e7847a98c7994926955f3
Etienne Laurent <etlaurent@tuta.io>
2022-05-06 15:22:08 +0300

    add fields

--- modules/@apostrophecms-pro/doc-type-scheduled-publishing/index.js
+++ modules/@apostrophecms-pro/doc-type-scheduled-publishing/index.js
@@ -1,3 +1,34 @@
 module.exports = {
-  improve: '@apostrophecms/doc-type'
+  improve: '@apostrophecms/doc-type',
+  fields(self) {
+    if (self.options.autopublish || self.options.localized === false) {
+      return;
+    }
+
+    return {
+      add: {
+        scheduledPublish: {
+          // TODO: use datetime
+          type: 'date',
+          label: 'scheduledPublish',
+          publishedLabel: 'scheduledUpdate'
+        },
+        scheduledUnpublish: {
+          // TODO: use datetime
+          type: 'date',
+          label: 'scheduledUnpublish'
+        }
+      },
+      group: {
+        utility: {
+          fields: [
+            'slug',
+            'visibility',
+            'scheduledPublish',
+            'scheduledUnpublish'
+          ]
+        }
+      }
+    };
+  }
 };
